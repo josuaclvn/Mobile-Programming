ff759c15a437184fb9c8b5f9a53629dffc745549
josuaclvn <josuaclvn@users.noreply.github.com>
2020-06-01 14:36:29 +0800

    UAS

---
+++ UAS/app/src/main/java/com/example/wyndhamdreamland/menu.java
@@ -0,0 +1,25 @@
+package com.example.wyndhamdreamland;
+
+public class menu {
+    private String nama;
+    private String harga;
+    private String gambar;
+
+    public menu(String datanama,String dataharga,String datagambar){
+        nama = datanama;
+        harga = dataharga;
+        gambar = datagambar;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+}
